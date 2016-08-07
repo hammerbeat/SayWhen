a1d58e44658504e0b2d978a9c8d0ebaeee40f3dd
hammerbeat <patrick@krings-mg.de>
2016-08-08 00:01:25 +0200

    primary weapon carousel is not rotating a lot!

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+     $.getJSON("./../json/assault.json")
+                .done(function(data)
+                      {
+                            var keys = Object.keys(data["primary"]);
+                            var weaponArray = keys.map(function (aKey) { return data["primary"][aKey] });
+                            //data is the array you expected.
+         var index = 0;
+         weaponArray.forEach(function(){
+             showCarousel(weaponArray,index);
+             index++;
+         });
+         //showCarousel(weaponArray, 0);
+                      }
+
+                     )
+         .fail(function(error, a, b){
+                            console.log("fail"+a);
+     })
+
+
+
+    function showCarousel(arr, index){
+    if(index >= arr.length) index = 0;
+
+    var item = arr[index];
+
+    if(index === 0) {
+        $(".carousel-inner").append($('<div class="item active"><img src="'+item.image+'" style="max-width:147px"><h4>'+item.name+'</h4></div></div>'));
+    } else {
+        $(".carousel-inner").append($('<div class="item"><img src="'+item.image+'" style="max-width:147px"><h4>'+item.name+'</h4></div></div>'));
+    }
+        console.log($(".carousel-inner"));
+
+    //set the timer
+    //setTimeout(function(){
+    //showCarousel(arr, index+1);
+    //}, 500);
+
+        }
+    $('.carousel').carousel({interval: 2500})
+});
