6911963afc2e245b50c4eb8730addf2f235e2dac
hammerbeat <patrick@krings-mg.de>
2016-08-08 11:53:18 +0200

    fixed most of the jslint warnings

--- js/main.js
+++ js/main.js
@@ -1,42 +1,34 @@
-$(document).ready(function() {
-     $.getJSON("./../json/assault.json")
-                .done(function(data)
-                      {
-                            var keys = Object.keys(data["primary"]);
-                            var weaponArray = keys.map(function (aKey) { return data["primary"][aKey] });
-                            //data is the array you expected.
-         var index = 0;
-         weaponArray.forEach(function(){
-             showCarousel(weaponArray,index);
-             index++;
-         });
-         //showCarousel(weaponArray, 0);
-                      }
+$(document).ready(function () {
+    $.getJSON("./../json/assault.json")
+        .done(function (data) {
+            var keys = Object.keys(data.primary);
+            var weaponArray = keys.map(function (aKey) { return data.primary[aKey]; });
+            //data is the array you expected.
+            var index = 0;
+            weaponArray.forEach(function () {
+                showCarousel(weaponArray, index);
+                index += 1;
+            });
+            //showCarousel(weaponArray, 0);
+        })
+        .fail(function (error, a, b) {
+            console.log("fail" + a);
+        });
 
-                     )
-         .fail(function(error, a, b){
-                            console.log("fail"+a);
-     })
-
-
-
-    function showCarousel(arr, index){
-    if(index >= arr.length) index = 0;
-
-    var item = arr[index];
-
-    if(index === 0) {
-        $(".carousel-inner").append($('<div class="item active"><img src="'+item.image+'" style="max-width:147px"><h4>'+item.name+'</h4></div></div>'));
-    } else {
-        $(".carousel-inner").append($('<div class="item"><img src="'+item.image+'" style="max-width:147px"><h4>'+item.name+'</h4></div></div>'));
-    }
+    function showCarousel(arr, index) {
+        if (index >= arr.length) { index = 0; }
+        var item = arr[index];
+        if (index === 0) {
+            $(".carousel-inner").append($('<div class="item active"><img src="' + item.image + '" style="max-width:147px"><h4>' + item.name + '</h4></div></div>'));
+        } else {
+            $(".carousel-inner").append($('<div class="item"><img src="' + item.image + '" style="max-width:147px"><h4>' + item.name + '</h4></div></div>'));
+        }
         console.log($(".carousel-inner"));
 
-    //set the timer
-    //setTimeout(function(){
-    //showCarousel(arr, index+1);
-    //}, 500);
-
-        }
-    $('.carousel').carousel({interval: 2500})
+        //set the timer
+        //setTimeout(function(){
+        //showCarousel(arr, index+1);
+        //}, 500);
+    }
+    $('.carousel').carousel({interval: 2500});
 });
